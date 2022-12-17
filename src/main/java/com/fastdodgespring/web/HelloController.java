2b8670041de72004e79a327dc600f33d53f1bb9c
정민규 <fastdodge7@naver.com>
2022-12-17 21:53:27 +0900

    First test code

---
+++ src/main/java/com/fastdodgespring/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.fastdodgespring.web;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController // Json 파일을 리턴하는 컨트롤러로 만들어줌
+public class HelloController {
+    @GetMapping("/hello") // HTTP Get 요청을 받을 수 있게 해 준다.
+    // 이제 /hello로 요청이 오면 문자열 hello를 반환하는 기능이 추가된 것.
+    public String hello(){
+        return "hello";
+    }
+}
