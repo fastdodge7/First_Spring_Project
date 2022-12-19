7dd9cac663457efb2681cddfe628aaa9026aadf9
정민규 <fastdodge7@naver.com>
2022-12-20 00:47:08 +0900

    DTO test

--- src/main/java/com/fastdodgespring/web/HelloController.java
+++ src/main/java/com/fastdodgespring/web/HelloController.java
@@ -1,7 +1,10 @@
 package com.fastdodgespring.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fastdodgespring.web.dto.HelloResponseDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Json 파일을 리턴하는 컨트롤러로 만들어줌
 public class HelloController {
@@ -9,5 +12,12 @@ public class HelloController {
     // 이제 /hello로 요청이 오면 문자열 hello를 반환하는 기능이 추가된 것.
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount)
+    {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/fastdodgespring/web/HelloControllerTest.java
+++ src/test/java/com/fastdodgespring/web/HelloControllerTest.java
@@ -1,4 +1,7 @@
-package fastdodgespring.web;
+package com.fastdodgespring.web;
+
+import com.fastdodgespring.web.HelloController;
+import com.fastdodgespring.web.dto.HelloResponseDto;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +15,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -24,6 +29,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returnsHelloDto() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount))) // param에는 무조건 string만 가능해서 int를 변환.
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", is(name)))
+        .andExpect(jsonPath("$.amount", is(amount)));
+
+        //$.name은 무엇인가? -> 아마 Json에서 부모 자식 관계가 있는 오브젝트가 포함된다면, 부모(루트)부터 쭉쭉 내려가야 하는 경우가
+        //있을 수 있는데, $란 문자는 루트노드를 나타내는 문자임. 지금은 그냥 오브젝트 하나에 name, amount 필드가 있으니, 루트에서
+        //부터 필드를 적는것.
     }
 
 }
